a8f277c7bc16ad438d67dc48f3c24d549424bd62
6etacat <michael.mao99@gmail.com>
2019-12-01 04:56:02 -0500

    update

--- xiaozhu.py
+++ xiaozhu.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters
 import logging
+import os
 
 groups = {}
 
@@ -26,9 +27,17 @@ def init_group(group_name):
         f.write(group_name)
 
 def update_group(group_name):
-    with open(f"groups/{group_name}.group", 'w') as f:
-        text = "\n".join([str(user_id) for user_id in groups[group_name]])
-        f.write(text)
+    if len(groups[group_name]) == 0:
+        with open(f"groups/index") as f:
+            group_names = set(f.read().strip().split('\n'))
+        group_names.remove(group_name)
+        with open(f"groups/index", 'w') as f:
+            f.write("\n".join(group_names))
+        os.remove(f"groups/{group_name}.group")
+    else:
+        with open(f"groups/{group_name}.group", 'w') as f:
+            text = "\n".join([str(user_id) for user_id in groups[group_name]])
+            f.write(text)
 
 def get_group_name(update):
     msg = update.message.text.strip().split()
